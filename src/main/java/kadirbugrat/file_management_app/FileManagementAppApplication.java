67cf9a87bd4e730d3fab2e1d679eb3261fe77d89
Kadir Bugra <kadirbugrat@gmail.com>
2025-05-22 00:29:58 +0300

    chore: import clean Spring Boot project structure

---
+++ src/main/java/kadirbugrat/file_management_app/FileManagementAppApplication.java
@@ -0,0 +1,13 @@
+package kadirbugrat.file_management_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FileManagementAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FileManagementAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kadirbugrat/file_management_app/FileManagementAppApplicationTests.java
@@ -0,0 +1,13 @@
+package kadirbugrat.file_management_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FileManagementAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
